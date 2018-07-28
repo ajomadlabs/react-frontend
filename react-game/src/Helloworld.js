23d058665643eea95d7e88569f71659c5ba43ebe
ajomadlabs <ajojohn555@gmail.com>
2018-07-28 14:53:31 +0530

    add interactivity via states and class based components

--- react-game/src/Helloworld.js
+++ react-game/src/Helloworld.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './Helloworld.css'
 
-const HelloWorld = props => {
-  return (
-    <div className="HelloWorld">Hello, {props.name}</div>
-  )
+class HelloWorld extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      greeting: 'Hello'
+    }
+    this.frenchify = this.frenchify.bind(this)
+  }
+  frenchify() {
+    this.setState({ greeting: 'Bonjour' });
+  }
+  render () {
+    return (
+      <div className="HelloWorld">
+        {this.state.greeting}, {this.props.name}
+        <br/>
+        <button onClick={this.frenchify}>Frenchify!</button>
+      </div>
+    )
+  }
 }
 
 export default HelloWorld
