11c610bb3be9e7d85a2d08db753e81ce5767ebfd
ajomadlabs <ajojohn555@gmail.com>
2018-07-28 14:27:51 +0530

    add styles to the App component

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
@@ -6,4 +7,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default App
