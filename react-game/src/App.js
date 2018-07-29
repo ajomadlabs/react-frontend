a3b4f6173f10990cffb199f0a1d2701edb19d811
ajomadlabs <ajojohn555@gmail.com>
2018-07-29 15:43:49 +0530

    get the value from input box

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -6,17 +6,32 @@ import './assets/css/Button.css'
 class App extends Component {
   constructor () {
     super()
+    this.state = {
+      boardSize: ''
+    }
     this.openModal = this.openModal.bind(this)
+    this.updateBoardSize = this.updateBoardSize.bind(this)
   }
 
   openModal () {
     let modal = document.getElementsByClassName('rg-modal')[0]
     modal.style.display = 'block'
+    let btn = document.getElementsByClassName('rg-modal-btn')[0]
+    btn.onclick = () => {
+      modal.style.display = 'none'
+    }
     window.onclick = (e) => {
       if (e.target === modal) {
         modal.style.display = 'none'
       }
     }
+  }
+
+  updateBoardSize (e) {
+    this.setState({
+      boardSize: e.target.value
+    })
+    console.log(this.state.boardSize)
   }
 
   render () {
@@ -30,7 +45,7 @@ class App extends Component {
           <div className="rg-modal-content">
             <div className="rg-modal-header">Board Size Input</div>
             <center>
-              <input type="text" className="rg-modal-input" placeholder="Enter Board Size"/>
+              <input type="text" value={this.state.boardSize} onChange={e => this.updateBoardSize(e)} className="rg-modal-input" placeholder="Enter Board Size"/>
             </center>
             <center>
               <div className="rg-modal-btn">Enter</div>
