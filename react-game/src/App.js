1e4503cf336e61045c4bd9812e0c0e8946a8db4d
ajomadlabs <ajojohn555@gmail.com>
2018-07-29 15:21:04 +0530

    make the pop up modal working

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Header from './components/HeaderComponent'
-import Button from './components/ButtonComponent'
-import Modal from './components/ModalComponent'
+import './assets/css/Modal.css'
+import './assets/css/Button.css'
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header title="React Maze Game"></Header>
-      <center>
-        <Button name="Play"></Button>
-      </center>
-      <Modal modalTitle="Board Size Input" modalButton="Enter"></Modal>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super()
+    this.openModal = this.openModal.bind(this)
+  }
+
+  openModal () {
+    let modal = document.getElementsByClassName('rg-modal')[0]
+    modal.style.display = 'block'
+    window.onclick = (e) => {
+      if (e.target === modal) {
+        modal.style.display = 'none'
+      }
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Header title="React Maze Game"></Header>
+        <center>
+          <div onClick={this.openModal} className="rg-btn">Play</div>
+        </center>
+        <div className="rg-modal">
+          <div className="rg-modal-content">
+            <div className="rg-modal-header">Board Size Input</div>
+            <center>
+              <input type="text" className="rg-modal-input" placeholder="Enter Board Size"/>
+            </center>
+            <center>
+              <div className="rg-modal-btn">Enter</div>
+            </center>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
